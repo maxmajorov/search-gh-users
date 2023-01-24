953ba94850348e00a251e8ddc43d182d947808fd
Max <maxpredko@gmail.com>
2023-01-24 11:21:53 +0300

    feat: create app for searching github users with using RxJS

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { SourceMapDevToolPlugin } = require("webpack");
+const path = require("path");
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html",
+  }),
+  new SourceMapDevToolPlugin({
+    filename: "[file].map",
+  }),
+];
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[hash:8].js",
+    path: path.join(__dirname, "/dist"),
+    sourceMapFilename: "[name].js.map",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 3200,
+  },
+  devtool: "source-map",
+  plugins,
+};
